ad78a4deb0ec9cb1c724a3fe0642a4b8f6708da4
thefoku <97766301+thefoku@users.noreply.github.com>
2022-11-09 02:35:00 +0400

    New testing JS

---
+++ roma.script.js
@@ -0,0 +1,10 @@
+ function test(){
+ const bread = document.querySelector('.ec-breadcrumbs')
+
+ const div = document.createElement('div')
+ div.innerHTML = `<p class='roma-timer'>Timer 00 : 00 : 00</p>`
+ div.className = 'azi'
+ 
+ bread.after(div)
+
+console.log('Button is clicked')}
