e8e72b9348857b758bd02b2cbec6769e0384d9fa
thefoku <97766301+thefoku@users.noreply.github.com>
2022-11-12 03:27:23 +0400

    Update timer.js

--- timer.js
+++ timer.js
@@ -9,3 +9,34 @@
 
 console.log('Button is clicked')}
  window.onload = test;
+
+function add(){
+ const bread = document.querySelector('.ec-likely')
+ const div = document.createElement('div')
+ div.innerHTML =  `<div class="additional-text" style="margin-top:20px;">
+ <p data-mce-fragment="1">
+ <i data-mce-fragment="1"></i>
+ <i data-mce-fragment="1">Prata kiteutrustning med ett proffs</i>
+ <br data-mce-fragment="1">
+ <strong>BESÖK VÅR BUTIK. MITT I CITY</strong>
+ <br data-mce-fragment="1">Vanadisvägen 21, Vasastan<br data-mce-fragment="1">
+ <strong>Måndag-Fredag:</strong>
+ <span data-mce-fragment="1"> </span>11.00 - 15.30. Kvällstid efter ö.k<br data-mce-fragment="1">
+ <strong>Lördag - Söndag:</strong><span data-mce-fragment="1">
+  </span>Boka besök<br data-mce-fragment="1">070 046 40 42 | 072 183 02 75<br data-mce-fragment="1"><a rel="nofollow" href="mailto:info@kiteboardcenter.se" data-mce-fragment="1" data-mce-href="mailto:info@kiteboardcenter.se" target="_blank">info@kiteboardcenter.se</a>
+</p>
+</div>`
+ div.className = 'added-text'
+ bread.after(div)
+console.log('Add function has loaded')};
+//Ecwid.OnPageLoaded.add(function() {
+//    console.log('test onload')
+//});
+window.onload = function pik(){
+console.log('Pikk')
+add()
+}
+document.addEventListener('DOMContentLoaded', function() {
+     console.log('test onload with DOM')
+}, false);
+
